2ed96b9060ebb8ca7c94cee79c57c4a87f475e4c
Adar <adar.hefer@gmail.com>
2023-01-31 11:24:44 +0200

    Publish fix attempt

--- trigger_gifted_subscription.sh
+++
@@ -1,4 +0,0 @@
-twitch event trigger channel-gift \
--t 120686446 \
--F https://ass.coursequery.app/twitch/eventsub/event/channel.subscription.gift.120686446 \
--s channel.subscription.gift.120686446.kfH529YAsf5U4nSdQXhTvDpF9zfYH7PEDK5KAsR4
--- trigger_subscription_event.sh
+++
@@ -1,4 +0,0 @@
-twitch event trigger subscribe-message \
--t 120686446 \
--F https://ass.coursequery.app/twitch/eventsub/event/channel.subscription.message.120686446 \
--s channel.subscription.message.120686446.kfH529YAsf5U4nSdQXhTvDpF9zfYH7PEDK5KAsR4
